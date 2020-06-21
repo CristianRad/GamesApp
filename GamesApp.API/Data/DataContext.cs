695e47b8bbfa68a1c49e121f3c1d980d4104567a
CristianRad <cristian.mihai.rad@gmail.com>
2020-06-21 12:44:27 +0300

    Initial Commit

---
+++ GamesApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using GamesApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
