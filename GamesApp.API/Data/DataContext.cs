6fce6a42e69d47f791b723f90193d73c0abba7a4
CristianRad <cristian.mihai.rad@gmail.com>
2020-06-21 21:49:34 +0300

    Added registration and login

--- GamesApp.API/Data/DataContext.cs
+++ GamesApp.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace GamesApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
